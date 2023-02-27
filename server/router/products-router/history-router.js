a6e19164282b05626ed71faa65e0e5708c312378
AntonSkalaban <112729189+AntonSkalaban@users.noreply.github.com>
2023-02-27 21:19:05 +0300

    fix: fix routring 2

--- server/router/products-router/history-router.js
+++ server/router/products-router/history-router.js
@@ -3,10 +3,10 @@ const HistoryController = require('../../controllers/products-controller/history
 
 const historyRouter = new Router();
 
-creditRouter.post('/history', HistoryController.create);
-creditRouter.get('/history/:user_id', HistoryController.getAll);
-creditRouter.get('/history/:id', HistoryController.getOne);
-creditRouter.put('/history/:id', HistoryController.update);
-creditRouter.delete('/history/:id', HistoryController.delete);
+historyRouter.post('/history', HistoryController.create);
+historyRouter.get('/history/:user_id', HistoryController.getAll);
+historyRouter.get('/history/:id', HistoryController.getOne);
+historyRouter.put('/history/:id', HistoryController.update);
+historyRouter.delete('/history/:id', HistoryController.delete);
 
 module.exports = historyRouter;
